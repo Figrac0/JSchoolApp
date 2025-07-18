f9dbc0fb86d3b7c905ecd7261632c30ab4ca00aa
sergey <serjjiniuss@gmail.com>
2025-07-18 13:21:55 +0400

    first commit

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+/* eslint-env node */
+module.exports = function (api) {
+    api.cache(true);
+    return { presets: ["babel-preset-expo"] };
+};
---
+++ shared/tokens.ts
@@ -0,0 +1,24 @@
+export const Colors = {
+	black: "#16171D",
+	gray: "#AFB2BF",
+	violetDark: "#2E2D3D",
+	primary: "#D86000",
+	primaryHover: "#F99110",
+	link: "#A97BFF",
+	white: "#FAFAFA",
+	red: "#CC384E",
+};
+
+export const Gaps = {
+	g16: 16,
+	g50: 50,
+};
+
+export const Radius = {
+	r10: 10,
+};
+
+export const Fonts = {
+	f16: 16,
+	f18: 18,
+};
